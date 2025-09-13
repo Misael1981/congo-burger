03859fccd5c2619aedd4bc04b858521c24fea415
Misael <misaelborges1981@gmail.com>
2025-09-13 18:59:19 -0300

    setup inicial

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page of the application.</p>
+      <Button>Bora pedir</Button>
+    </div>
+  );
+};
+
+export default HomePage;
