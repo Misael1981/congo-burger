22b005996ed8393d5ea482571d1b4ac6fc7aaab6
Misael <misaelborges1981@gmail.com>
2025-09-14 20:08:22 -0300

    ProductDetails

---
+++ src/app/[slug]/menu/[productId]/components/ProductDetails/index.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+const ProductDetails = ({ product, restaurant }) => {
+  const [quantity, setQuantity] = useState(0);
+  const handleDrecrement = () => {
+    setQuantity((prevQuantity) => {
+      if (prevQuantity === 0) {
+        return 0;
+      }
+      return prevQuantity - 1;
+    });
+  };
+  const handleIncrement = () => {
+    setQuantity((prevQuantity) => prevQuantity + 1);
+  };
+  return (
+    <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
+      <div className="flex-auto overflow-hidden">
+        <div className="mt-2 flex items-center gap-1.5">
+          <Image
+            src={restaurant?.avatarImageUrl}
+            alt={restaurant?.name}
+            width={16}
+            height={16}
+            className="rounded-full"
+          />
+          <p className="text-xs text-muted-foreground">{restaurant?.name}</p>
+        </div>
+        <h2 className="text-xl font-semibold">{product?.name}</h2>
+
+        <div className="mt-3 flex items-center justify-between">
+          <h3 className="text-xl font-semibold text-green-500">
+            {formatCurrency(product.price)}
+          </h3>
+          <div className="flex items-center gap-3 text-center">
+            <Button
+              variant="outline"
+              className="h-8 w-8 rounded-xl"
+              onClick={handleDrecrement}
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <p className="w-4">{quantity}</p>
+            <Button
+              variant="destructive"
+              className="h-8 w-8 rounded-xl"
+              onClick={handleIncrement}
+            >
+              <ChevronRightIcon />
+            </Button>
+          </div>
+        </div>
+        <div className="mt-6 space-y-3">
+          <h4 className="font-semibold">Sobre</h4>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+        </div>
+        <div className="mt-6 space-y-3">
+          <div className="5 flex items-center gap-1">
+            <ChefHatIcon size={18} />
+            <h4 className="font-semibold">Ingredientes</h4>
+          </div>
+          <ul className="text-muted-fo list-disc px-5 text-sm text-muted-foreground">
+            {product.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <Button className="mt-6 w-full rounded-full">Adiciona à sacola</Button>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- src/app/[slug]/menu/[productId]/page.jsx
+++ src/app/[slug]/menu/[productId]/page.jsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import ProductsHeader from "./components/ProductsHeader";
+import ProductDetails from "./components/ProductDetails";
 
 const ProductPage = async ({ params }) => {
   const { slug, productId } = await params;
@@ -8,14 +9,28 @@ const ProductPage = async ({ params }) => {
     where: {
       id: productId,
     },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        },
+      },
+    },
   });
   if (!product) {
     return notFound("Produto não encontrado");
   }
 
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    return notFound();
+  }
+
   return (
     <>
       <ProductsHeader product={product} />
+      <ProductDetails product={product} restaurant={product.restaurant} />
     </>
   );
 };
